51d9cc538daed3bb3643ba80d329c2425f7868b3
Andre Luiz Gava <andrelzgava@gmail.com>
2026-01-02 01:37:05 -0300

    New drafts for classes

--- src/Compiler/Processors/FunctionHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PHPScript\Compiler\Processors;
-
-class FunctionHandler implements PreprocessorInterface {
-  public function process(string $code): string {
-    throw new \Exception('Not implemented');
-  }
-}
