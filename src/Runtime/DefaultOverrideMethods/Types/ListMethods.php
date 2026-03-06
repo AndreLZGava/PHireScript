c14f20e49ef81ab5f5c418ea15d0822129af674b
Andre Luiz Gava <andrelzgava@gmail.com>
2026-03-06 20:03:48 -0300

    Implemented new methods for list

--- src/Runtime/DefaultOverrideMethods/Types/ListMethods.php
+++ src/Runtime/DefaultOverrideMethods/Types/ListMethods.php
@@ -5,40 +5,68 @@ declare(strict_types=1);
 namespace PHireScript\Runtime\DefaultOverrideMethods\Types;
 
 use PHireScript\Runtime\DefaultOverrideMethods\BaseMethods;
+use PHireScript\Runtime\DefaultOverrideMethods\BaseParams;
 
-class ListMethods extends GeneralType
-{
+class ListMethods extends GeneralType {
     public function __construct(
         public array $types,
     ) {
     }
 
-    public function enqueue(...$params)
-    {
+    public function contains() {
         return new BaseMethods(
-            'enqueue!',
-            'array_push(@self, @params)',
-            [],
-            $this->types,
-            $params
+            name: 'contains?',
+            phpCodeForConversion: 'in_array(@searching, @self)',
+            returnOfPhpExecution: ['Bool'],
+            subTypes: $this->types,
+            params: [
+                new BaseParams(name: '@searching', type: 'mixed', required: true)
+            ]
         );
     }
 
-    public function dequeue()
-    {
+    public function keyDefined() {
         return new BaseMethods(
-            'dequeue!',
-            'array_shift(@self)',
-            [],
+            name: 'keyDefined?',
+            phpCodeForConversion: 'array_key_exists(@searching, @self)',
+            returnOfPhpExecution: ['Bool'],
+            subTypes: $this->types,
+            params: [
+                new BaseParams(name: '@searching', type: 'int', required: true)
+            ]
         );
     }
 
-    public function peek()
-    {
+    public function add() {
         return new BaseMethods(
-            'peek',
-            'reset(@self)',
-            ['self'],
+            name: 'add!',
+            phpCodeForConversion: '@self[@key] = @value',
+            returnOfPhpExecution: [],
+            subTypes: [],
+            params: [
+                new BaseParams(name: '@value', type: 'mixed', required: true),
+                new BaseParams(name: '@key', type: 'int', required: false, defaultValue: null, relatedKeyParam: true),
+            ]
+        );
+    }
+
+    public function last() {
+        return new BaseMethods(
+            name: 'last',
+            phpCodeForConversion: 'empty(@self) ? null : @self[array_key_last(@self)];',
+            returnOfPhpExecution: ['Mixed'],
+            subTypes: $this->types,
+            params: [],
+        );
+    }
+
+    public function first() {
+        return new BaseMethods(
+            name: 'first',
+            phpCodeForConversion: 'current(@self ?? [])',
+            returnOfPhpExecution: ['Mixed'],
+            subTypes: $this->types,
+            params: [],
         );
     }
 }
